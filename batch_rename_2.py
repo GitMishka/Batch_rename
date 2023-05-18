b883e1033b209dff4074711a0ae87f7dccd2b531
Misha <mishagrigoryan@gmail.com>
2023-05-17 22:52:05 -0400

    rename file + order

---
+++ batch_rename_2.py
@@ -0,0 +1,24 @@
+import os
+
+
+directory = input("Enter the directory path where files need to be renamed: ")
+
+
+new_name = input("Enter the new name for files: ")
+
+i = 1
+
+for filename in os.listdir(directory):
+    # Get the file extension
+    file_extension = os.path.splitext(filename)[1]
+
+    new_filename = f"{new_name}{i}{file_extension}"
+    
+    source = os.path.join(directory, filename)
+    destination = os.path.join(directory, new_filename)
+    
+    os.rename(source, destination) 
+
+    i += 1
+
+print("Files renamed successfully!")
